45df476e09ffdaccf638c9d0e8a6df8657119365
陈亦涛 <chenyitao@tuia.cn>
2020-04-05 17:32:50 +0800

    create canvas dom

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import * as PIXI from 'pixi.js'
+import './styles/reset.less'
 
-console.log(PIXI)
+const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    antialias: true,
+    transparent: true,
+  }
+)
+
+document.body.appendChild(app.view)
+
+window.onresize = () => {
+  app.renderer.resize(window.innerWidth, window.innerHeight)
+}
