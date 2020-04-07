88f48632dfa48937fef1df8378f0b5d34814aea6
陈亦涛 <chenyitao@tuia.cn>
2020-04-07 09:59:48 +0800

    render pikachu sprite into app

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import * as PIXI from 'pixi.js'
 import './styles/reset.less'
+import pikachuImage from './images/pikachu.png'
 
+const TextureCache = PIXI.utils.TextureCache
+const Sprite = PIXI.Sprite
+const loader = new PIXI.Loader()
 const app = new PIXI.Application({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -8,9 +12,21 @@ const app = new PIXI.Application({
     transparent: true,
   }
 )
-
 document.body.appendChild(app.view)
-
-window.onresize = () => {
-  app.renderer.resize(window.innerWidth, window.innerHeight)
+app.renderer.autoResize = true
+window.onresize = () => app.renderer.resize(window.innerWidth, window.innerHeight)
+const textures = {
+  pikachu: pikachuImage
 }
+Object.entries(textures).forEach(([name, path]) => loader.add(name, path))
+loader.on('complete', (loader, resources) => {
+  console.log(resources)
+  const pikachu = new Sprite(resources.pikachu.texture)
+  app.stage.addChild(pikachu)
+  setTimeout(() => {
+    pikachu.visible = false
+  }, 2000)
+})
+loader.load()
+
+
